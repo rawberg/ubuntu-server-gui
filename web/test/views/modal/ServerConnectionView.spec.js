6a3b23265c509cdb493479d377cacafe8b0d9ac4
David Feinberg <david@rawberg.com>
2013-07-07 23:47:36 -0700

    - fixed ServerConnectionView specs

--- web/test/views/modal/ServerConnectionView.spec.js
+++ web/test/views/modal/ServerConnectionView.spec.js
@@ -1,44 +1,40 @@
 define(function (require_browser) {
-    // Libs
-    var $ = require_browser('jquery'),
-        _ = require_browser('underscore'),
-        Backbone = require_browser('backbone'),
-        Marionette = require_browser('marionette'),
-        App = require_browser('App'),
     // Models
-        Server = require_browser('models/Server'),
+    var Server = require_browser('models/Server'),
+        ServerConnection = require_browser('models/ServerConnection'),
     // Views
-        ServerConnectionModal = require_browser('views/modal/ServerConnectionView');
+        ServerConnectionView = require_browser('views/modal/ServerConnectionView');
 
-    describe('ServerConnection Modal', function() {
+    describe('ServerConnectionView', function() {
         describe('onRender', function() {
+            var server, serverConnection;
+            var serverConnectionView;
 
-            var serverConnectionModal, serverConnectionModel;
             beforeEach(function() {
-                var server = new Server({ipv4: '10.0.0.1', name: 'Simple Server'});
-                serverConnectionModel = new Backbone.Model(_.extend({connection_status: 'connecting'}, server.toJSON()), {});
-                serverConnectionModal = new ServerConnectionModal({model: serverConnectionModel});
-                serverConnectionModal.render();
+                server = new Server({ipv4: '10.0.0.1', name: 'Simple Server'});
+                serverConnection = new ServerConnection({connection_status: 'connecting'}, {server: server});
+                serverConnectionView = new ServerConnectionView({model: serverConnection});
+                serverConnectionView.render();
             });
 
             afterEach(function() {
-                serverConnectionModal.close();
+                serverConnectionView.close();
             });
 
             it('displays connecting status and server name in the modal', function() {
-                (serverConnectionModal.$('h3').text()).should.have.string('connecting');
-                (serverConnectionModal.$('.modal-body').text()).should.have.string('Simple Server');
-                (serverConnectionModal.$('div.modal-body').hasClass('connecting')).should.be.true;
+                (serverConnectionView.$('h3').text()).should.have.string('connecting');
+                (serverConnectionView.$('.modal-body').text()).should.have.string('Simple Server');
+                (serverConnectionView.$('div.modal-body').hasClass('connecting')).should.be.true;
             });
 
             it('displays connection error message in the modal', function() {
-                serverConnectionModel.set('connection_status', 'connection error');
-                (serverConnectionModal.$('h3').text()).should.have.string('connection error');
+                serverConnection.set('connection_status', 'connection error');
+                (serverConnectionView.$('h3').text()).should.have.string('connection error');
             });
 
             it('displays the correct template on connect error', function() {
-                serverConnectionModel.set('connection_status', 'connection error');
-                (serverConnectionModal.$('div.modal-body').hasClass('connection-error')).should.be.true;
+                serverConnection.set('connection_status', 'connection error');
+                (serverConnectionView.$('div.modal-body').hasClass('connection-error')).should.be.true;
             });
         });
     });
