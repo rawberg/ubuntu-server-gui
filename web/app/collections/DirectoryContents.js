b9dba4359a06c365fd2100ad9e7a112e5f5e4082
David Feinberg <david@rawberg.com>
2013-09-12 01:07:55 -0700

    Added sorting capabilities to DirectoryContents collection.

--- web/app/collections/DirectoryContents.js
+++ web/app/collections/DirectoryContents.js
@@ -1,7 +1,6 @@
 define(function (require_browser, exports, module) {
-    var $ = require_browser('jquery'),
-        _ = require_browser('underscore'),
-        Backbone = require_browser('backbone');
+    var Backbone = require_browser('backbone'),
+        _ = require_browser('underscore');
 
     var DirectoryItem = Backbone.Model.extend({
         parse: function(response, options) {
@@ -18,7 +17,8 @@ define(function (require_browser, exports, module) {
 
     var DirectoryContents = module.exports.DirectoryContents = Backbone.Collection.extend({
         model: DirectoryItem,
-        comparator: 'filename',
+        sortProperty: '',
+        sortDirection: 'ASC',
 
         initialize: function(models, options) {
             this.server = (options && options.server) ? options.server : null;
@@ -27,12 +27,66 @@ define(function (require_browser, exports, module) {
             }
         },
 
+        comparator: function(modelOne, modelTwo, otherStuff) {
+            return this[this.sortProperty + 'Sort'].call(this, modelOne, modelTwo);
+        },
+
         fetch: function(options) {
             var path = (options && options.path) ? options.path : '/';
             this.server.sshProxy.usgOpendir(path, _.bind(function (err, list) {
+                console.log(list);
                 this.add(list, {parse: true});
-                this.sort();
             }, this));
+        },
+
+        filenameSort: function(modelOne, modelTwo) {
+            if(this.sortDirection === 'ASC') {
+                return (modelOne.get('filename')) > (modelTwo.get('filename')) ? 1 : -1;
+            } else {
+                return (modelOne.get('filename')) < (modelTwo.get('filename')) ? 1 : -1;
+            }
+        },
+
+        mtimeSort: function(modelOne, modelTwo) {
+            if (this.sortDirection === 'ASC') {
+                return (modelOne.get('mtime')) > (modelTwo.get('mtime')) ? 1 : -1;
+            } else {
+                return (modelOne.get('mtime')) < (modelTwo.get('mtime')) ? 1 : -1;
+            }
+        },
+
+        sizeSort: function(modelOne, modelTwo) {
+            if (this.sortDirection === 'ASC') {
+                return (modelOne.get('size')) > (modelTwo.get('size')) ? 1 : -1;
+            } else {
+                return (modelOne.get('size')) < (modelTwo.get('size')) ? 1 : -1;
+            }
+        },
+
+        sort: function (options) {
+            if (!this.comparator) throw new Error('Cannot sort a set without a comparator');
+            options || (options = {});
+
+            var currentSortProperty = this.sortProperty;
+            this.sortProperty = options.sortProperty ? options.sortProperty : 'filename';
+
+            if (currentSortProperty === this.sortProperty) {
+                this.sortDirection = 'DESC';
+            } else {
+                this.sortDirection = 'ASC';
+            }
+            this.sortDirection = options.sortDirection ? options.sortDirection : this.sortDirection;
+
+            // Run sort based on type of `comparator`.
+            if (_.isString(this.comparator) || this.comparator.length === 1) {
+                this.models = this.sortBy(this.comparator, this);
+            } else {
+                this.models.sort(_.bind(this.comparator, this));
+            }
+
+            if (!options.silent) this.trigger('sort', this, options);
+            return this;
         }
+
     });
 });
--- web/test/collections/DirectoryContents.spec.js
+++ web/test/collections/DirectoryContents.spec.js
@@ -1,25 +1,70 @@
 define(function (require_browser) {
     var App = require_browser('App'),
-        DirectoryContents = require_browser('collections/DirectoryContents');
+        DirectoryContents = require_browser('collections/DirectoryContents').DirectoryContents;
 
     describe('DirectoryContents - Collection', function() {
 
-        describe('fetch', function() {
+        describe('sort', function() {
             var directoryContents;
+            var sampleData = [{
+                filename: 'one-file.jpg',
+                size: 434343,
+                mtime: 1360468126   // 2/9/2013 7:48:46 PM
+            },{
+                filename: 'sample-file.png',
+                size: 100000,
+                mtime: 1366266020   // 4/17/2013 11:20:20 PM
+            },{
+                filename: 'a-first-file.ini',
+                size: 444444,
+                mtime: 1378969110   // 9/11/2013 11:58:30 PM
+            },{
+                filename: 'z-last-file.config',
+                size: 999999,
+                mtime: 1330970070   // 3/5/2012 9:54:30 AM
+            }];
 
             beforeEach(function() {
-                sftpOpenDirSpy = sinon.stub(DirectoryContents.prototype, 'sftpOpenDir');
-                directoryContents = new DirectoryContents();
+                directoryContents = new DirectoryContents(sampleData, {server: {}});
             });
 
             afterEach(function() {
-                sftpOpenDirSpy.restore();
                 directoryContents.reset([], {silent: true});
             });
 
-            xit('should call sftpOpenDir', function() {
-                directoryContents.fetch();
-                (sftpOpenDirSpy).should.have.been.called;
+            it('triggers onSort when data is sorted', function() {
+                var sortEventSpy = sinon.spy();
+                directoryContents.on('sort', sortEventSpy);
+
+                sortEventSpy.should.not.have.been.called;
+                directoryContents.sort({sortProperty: 'mtime'});
+                sortEventSpy.should.have.been.called;
+                (sortEventSpy.args[0][1]['sortProperty']).should.equal('mtime');
+            });
+
+            it('default sorts by filename (ASC)', function() {
+                (directoryContents.first().get('filename')).should.equal('a-first-file.ini');
+                (directoryContents.last().get('filename')).should.equal('z-last-file.config');
+            });
+
+            it('sorts by file size (ASC) and (DSC)', function() {
+                directoryContents.sort({sortProperty: 'size'});
+                (directoryContents.first().get('size')).should.equal(100000);
+                (directoryContents.last().get('size')).should.equal(999999);
+
+                directoryContents.sort({sortProperty: 'size'});
+                (directoryContents.first().get('size')).should.equal(999999);
+                (directoryContents.last().get('size')).should.equal(100000);
+            });
+
+            it('sorts by modified time (ASC) and (DSC)', function () {
+                directoryContents.sort({sortProperty: 'mtime'});
+                (directoryContents.first().get('mtime')).should.equal(1330970070);
+                (directoryContents.last().get('mtime')).should.equal(1378969110);
+
+                directoryContents.sort({sortProperty: 'mtime'});
+                (directoryContents.first().get('mtime')).should.equal(1378969110);
+                (directoryContents.last().get('mtime')).should.equal(1330970070);
             });
 
         });
