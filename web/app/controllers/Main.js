ba71d68bfeba4e7d2cecf1cc01c0fc77c3840d48
David Feinberg <david@rawberg.com>
2014-01-09 22:09:46 -0800

    Removed left sidebar cruft.

--- web/app/controllers/Main.js
+++ web/app/controllers/Main.js
@@ -2,30 +2,14 @@ define(function (require_browser) {
     var _ = require_browser('underscore'),
         App = require_browser('App'),
         BaseController = require_browser('controllers/Base'),
-
         DashboardLayout = require_browser('views/dashboard/Dashboard').DashboardLayout,
         FileManagerLayout = require_browser('views/filemanager/FileManager').FileManagerLayout,
-
-        LeftSidebarView = require_browser('views/dashboard/LeftSidebar').LeftSidebar,
         ServerListCollection = require_browser('collections/ServerList');
 
     return BaseController.extend({
         initialize: function() {
             this.App = App;
             BaseController.prototype.initialize.apply(this, arguments);
-        },
-
-        _sidebarLeft: function(sidebarRegion) {
-            var serverList = new ServerListCollection(),
-                leftsidebarView = new LeftSidebarView({collection: serverList});
-
-            sidebarRegion.show(leftsidebarView);
-
-            serverList.fetch({success: _.bind(function() {
-                if(serverList.length === 0) {
-                    this.App.vent.trigger('noobtour:activate');
-                }
-            }, this)});
         },
 
         dashboard: function() {
--- web/test/controllers/Main.spec.js
+++ web/test/controllers/Main.spec.js
@@ -11,7 +11,6 @@ define(function (require_browser) {
 
         // Views
         DashboardLayout = require_browser('views/dashboard/Dashboard').DashboardLayout,
-        LeftSidebarView = require_browser('views/dashboard/LeftSidebar').LeftSidebar,
         PlatformStatsView = require_browser('views/dashboard/PlatformStats'),
         RunningServicesView = require_browser('views/dashboard/RunningServices'),
         UtilizationStatsView = require_browser('views/dashboard/UtilizationStats');
@@ -19,50 +18,29 @@ define(function (require_browser) {
     describe('Main - Controller', function() {
 
         describe('dashboard', function() {
-            var mainController, dashboardLayoutSpy, leftsidebarSpy;
-            var dashboardLayout, leftSidebar, runningServices, footerPosStub;
+            var mainController, dashboardLayoutSpy;
+            var dashboardLayout, runningServices, footerPosStub;
 
             beforeEach(function() {
                 footerPosStub = sinon.stub($.prototype, 'offset');
                 footerPosStub.returns({top: 666});
-                leftsidebarSpy = sinon.stub(LeftSidebarView.prototype, 'render');
                 dashboardLayoutSpy = sinon.stub(DashboardLayout.prototype, 'render');
 
                 mainController = new MainController();
-
                 sinon.spy(mainController.App.mainViewport, 'show');
-                sinon.spy(mainController.App.mainToolbar, 'show');
-                sinon.spy(mainController.App.mainFooterbar, 'show');
 
                 mainController.dashboard();
                 dashboardLayout = mainController.App.mainViewport.currentView;
-                leftSidebar = dashboardLayout.sidebarLeftRegion.currentView;
             });
 
             afterEach(function() {
                 footerPosStub.restore();
-                leftsidebarSpy.restore();
                 dashboardLayoutSpy.restore();
-
                 mainController.App.mainViewport.show.restore();
-                mainController.App.mainToolbar.show.restore();
-                mainController.App.mainFooterbar.show.restore();
-            });
-
-            xit('should show mainToolbar', function() {
-                (mainController.App.mainToolbar.show).should.have.been.called;
-            });
-
-            xit('should show mainFooterbar', function() {
-                (mainController.App.mainFooterbar.show).should.have.been.called;
             });
 
             it('should show mainViewport', function() {
                 (mainController.App.mainViewport.show).should.have.been.called;
-            });
-
-            xit('should show leftSidebar', function() {
-                (leftsidebarSpy).should.have.been.called;
             });
 
             it("should show the dashboard layout", function() {
