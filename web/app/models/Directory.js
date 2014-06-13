0d39a31be53f5ca93c0cc2fa5b7f3e8e83046be1
David Feinberg <david@rawberg.com>
2014-06-12 20:04:22 -0700

    converted Directory model module style from simplified commonjs to standard requirejs (r.js compatibility #12)

---
+++ web/app/models/Directory.js
@@ -0,0 +1,21 @@
+define(['backbone', 'underscore'], function (backbone, underscore) {
+
+    return Backbone.Model.extend({
+        parse: function(response, options) {
+            try {
+                result = {
+                    filename: response.filename,
+                    mode: response.attrs.mode,
+                    atime: response.attrs.atime,
+                    mtime: response.attrs.mtime,
+                    size: response.attrs.size,
+                    permissions: response.attrs.permissions
+                }
+            }
+            catch(e) {
+                result = {};
+            }
+            return result;
+        }
+    });
+});
