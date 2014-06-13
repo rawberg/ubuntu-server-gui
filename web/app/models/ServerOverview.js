e9072d6f9a0ac3b8f73287b66590719299966407
David Feinberg <david@rawberg.com>
2014-06-12 20:15:15 -0700

    converted ServerOverview module style from simplified commonjs to standard requirejs (r.js compatibility #12)

--- web/app/models/ServerOverview.js
+++ web/app/models/ServerOverview.js
@@ -1,32 +1,33 @@
-define([
-    'underscore',
-    'backbone',
-    'App'],
-    function(_, Backbone, App) {
-        return Backbone.Model.extend({
-            defaults: {
-                'memory': 0,
-                'tcp_connections': 0
-            },
+define(['underscore',
+        'backbone',
+        'App'], function(
+        _,
+        Backbone,
+        App) {
 
-            initialize: function(attributes, options) {
-                this.remote = true;
-                this.server = (options && options.server) ? options.server : null;
+    return Backbone.Model.extend({
+        defaults: {
+            'memory': 0,
+            'tcp_connections': 0
+        },
+
+        initialize: function(attributes, options) {
+            this.remote = true;
+            this.server = (options && options.server) ? options.server : null;
+            this.fetch();
+            setInterval(_.bind(function() {
                 this.fetch();
-                setInterval(_.bind(function() {
-                    this.fetch();
-                }, this), 300000);
-            },
+            }, this), 300000);
+        },
 
-            fetch: function() {
-                this.server.sshProxy.usgExec("free | grep Mem | awk '{print $3/$2 * 100.0}'", {}, _.bind(function(data, extended) {
-                    this.set('memory', Number(parseFloat(data).toFixed(1)));
-                }, this));
+        fetch: function() {
+            this.server.sshProxy.usgExec("free | grep Mem | awk '{print $3/$2 * 100.0}'", {}, _.bind(function(data, extended) {
+                this.set('memory', Number(parseFloat(data).toFixed(1)));
+            }, this));
 
-                this.server.sshProxy.usgExec("netstat -t | sed -n '/^tcp\ /p' |  awk 'END { print NR }'", {}, _.bind(function(data, extended) {
-                    this.set('tcp_connections', parseInt(data));
-                }, this));
-            }
-        });
-    }
-);
+            this.server.sshProxy.usgExec("netstat -t | sed -n '/^tcp\ /p' |  awk 'END { print NR }'", {}, _.bind(function(data, extended) {
+                this.set('tcp_connections', parseInt(data));
+            }, this));
+        }
+    });
+});
