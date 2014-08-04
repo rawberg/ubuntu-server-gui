2edbd334a701e627a389465785ed1c63fe9887fc
David Feinberg <david@rawberg.com>
2014-08-03 21:20:05 -0700

    fixed Editor integration test flakiness

--- web/tests/app-integration/Editor.int.js
+++ web/tests/app-integration/Editor.int.js
@@ -8,6 +8,7 @@ module.exports = {
             .waitForElementNotPresent('.modal-body.connecting', 4000)
             .click('.toolbar-file_cabinet a')
             .waitForElementPresent('.directory-explorer', 2000, 'directory explorer displays')
+            .pause(1000)
     },
 
     'file editor displays contents of an existing file' : function (browser) {
@@ -34,16 +35,11 @@ module.exports = {
     },
 
     'file editor displays edits and saves an existing file' : function (browser) {
-        var windowHandle;
-        browser.window_handle(function(result) {
-            windowHandle = result;
-        });
         browser
             .click('xpath', '//table//td[@class="filename"][text()="vagrant"]')
             .waitForElementPresent('.directory-explorer tbody', 2000, 'directory explorer shows vagrant folder contents')
             .click('xpath', '//table//td[@class="filename"][text()="Z01-existing_file.txt"]')
             .waitForElementPresent('#editor_layout', 3000, 'containing layout for code mirror displays')
-            .pause(1000)
             .assert.elementPresent('.CodeMirror-lines', 'displays code mirror editor')
             .assert.containsText('.CodeMirror-lines pre span', 'existing content', 'existing file contents is shown in the editor')
             .click('.CodeMirror-lines')
@@ -62,8 +58,8 @@ module.exports = {
             ])
             .keys([browser.Keys.META, 's'])
             .keys([browser.Keys.META])
-            .click('.toolbar-file_cabinet a')
-            .waitForElementPresent('.breadcrumb', 2000, 'wait for breadcrumb bar')
+            .click('.file-close')
+            .waitForElementPresent('.breadcrumb', 3000, 'wait for breadcrumb bar')
             .assert.containsText('.breadcrumb li:last-child span', '/', 'furthest breadcrumb is /')
             .click('xpath', '//table//td[@class="filename"][text()="vagrant"]')
             .waitForElementPresent('.directory-explorer tbody', 2000, 'directory explorer shows vagrant folder contents')
@@ -78,12 +74,12 @@ module.exports = {
                 'existing'
             ])
             .keys([browser.Keys.META, 's'])
-            .keys(['w'])
             .keys([browser.Keys.META])
-            .waitForElementPresent('.breadcrumb', 2500, 'wait for breadcrumb bar')
+            .click('.file-close')
+            .waitForElementPresent('.breadcrumb', 3000, 'wait for breadcrumb bar')
             .assert.containsText('.breadcrumb li:last-child span', '/', 'furthest breadcrumb is /')
             .click('xpath', '//table//td[@class="filename"][text()="vagrant"]')
-            .waitForElementPresent('.directory-explorer tbody', 2500, 'directory explorer shows vagrant folder contents')
+            .waitForElementPresent('.directory-explorer tbody', 2000, 'directory explorer shows vagrant folder contents')
             .click('xpath', '//table//td[@class="filename"][text()="Z01-existing_file.txt"]')
             .assert.containsText('.CodeMirror-lines pre span', 'existing content', 'existing file contents is shown in the editor')
             .end()
