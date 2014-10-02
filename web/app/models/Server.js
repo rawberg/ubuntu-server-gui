9da9549752cbea951ee57d54f7134449b02063de
David Feinberg <david@rawberg.com>
2014-10-01 22:31:29 -0700

    added connection management methods to Server model

--- web/app/models/Server.js
+++ web/app/models/Server.js
@@ -1,14 +1,17 @@
 define(['underscore',
         'backbone',
+        'App',
+        'models/ServerConnection',
         'backbone_dualstorage'], function (
         _,
-        Backbone) {
+        Backbone,
+        App,
+        ServerConnection) {
 
     return Backbone.Model.extend({
         url: 'Servers',
         local: true,
         connection: undefined,
-        sshProxy: undefined,
         sftpProxy: undefined,
         defaults: {
             id: null,
@@ -25,6 +28,29 @@ define(['underscore',
 
         getUrl: function() {
             return 'http://' + this.get('ipv4') + ':' + this.get('port');
+        },
+
+        connect: function() {
+            if(typeof this.connection === 'undefined') {
+                this.connection = new ServerConnection({}, {server: this});
+            }
+            this.connection.set('connection_status', 'connecting');
+            App.connectionModal(this);
+            this.connection.connect();
+        },
+
+        disconnect: function() {
+
+        },
+
+        isConnected: function() {
+            if(typeof this.connection === "undefined") {
+                return false;
+            } else if(this.connection.get('connection_status') !== 'connected') {
+                return false;
+            } else if(this.connection.sshProxy._state !== 'closed') {
+                return true;
+            }
         },
 
         setDefaultKeyPath: function() {
